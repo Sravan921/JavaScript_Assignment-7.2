0f2f7967e20ccbe1611a0b48603a9ac8a80041a8
Sravan921 <sukhavasi92@gmail.com>
2016-11-30 00:55:00 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+function findSum()
+{ 
+
+document.write ("Sum of the Two passed arguments is : " + (arguments[0] + arguments[1]));  //It will display sum of the two passed argument values.
+
+}
+
+findSum(10,15); 
+
+
